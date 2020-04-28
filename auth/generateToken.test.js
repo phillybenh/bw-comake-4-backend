4ae4d02526637193e96d45a12b230405c9d43e60
Sara Reidy <sara.reidy.21@gmail.com>
2020-04-28 10:07:41 -0400

    Fixing auth issues

--- auth/generateToken.test.js
+++ auth/generateToken.test.js
@@ -1,5 +1,10 @@
 const generateToken = require('./generateToken.js');
 
 describe('generateToken', () => {
-    it('')
+    it('should return a JSON web token when given user information', () => {
+        expect(generateToken({id: 1, username: 'vader'})).toBeTruthy()
+    })
+    it('should not return any user information', () => {
+        expect(generateToken({id:1, username: 'vader'})).toEqual(expect.not.stringContaining('vader'))
+    } )
 })
